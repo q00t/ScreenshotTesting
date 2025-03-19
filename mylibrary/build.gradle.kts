817e27b264c5d1e957b616cf7d77fccfaee92b8d
Simon Haley <12799329+q00t@users.noreply.github.com>
2025-03-19 14:00:38 +0000

    Updated libraries and created baselines

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.ui)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 
